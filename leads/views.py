8631a1ae0679f5bd826cd4294fab1d0c39966da7
kingbeebop <kingbeebop@gmail.com>
2023-04-21 11:02:16 -0400

    minor code reorganization

--- leads/views.py
+++ leads/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 import requests
 
 #/leads/ display json of all leads
+#intended for internal use only
 @api_view(['GET', 'POST'])
 def lead_list(request):
 
@@ -22,6 +23,7 @@ def lead_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 #/leads/id get, update, delete requests
+#intended for internal use only
 @api_view(['GET', 'POST', 'DELETE'])
 def lead_detail(request, id):
     
@@ -48,6 +50,8 @@ def lead_detail(request, id):
 @api_view(['GET'])
 def lead_inbox(request, api_key):
 
+    #api validation, find firm by api key
+    #not secure
     #TODO: use built in api keys instead
     try:
         firm = Firm.objects.get(api_key = api_key)
@@ -60,11 +64,11 @@ def lead_inbox(request, api_key):
             print(lead)
         except Lead.DoesNotExist:
             return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        lead.processed = True
-        lead.save()
+
+        #serialize data
         serializer = LeadSerializer(lead)
 
+        #send POST request to clio with message data
         clio_response = requests.post(
             'https://grow.clio.com/inbox_leads',
             data = {"inbox_lead": serializer.data,
@@ -72,4 +76,9 @@ def lead_inbox(request, api_key):
                     headers = {"Content-Type": "application/json",
                                "Accept": "application/json"})
         
+        #mark new message as processed
+        lead.processed = True
+        lead.save()
+        
+        #return json with sent data and a copy of the response from the clio api call above
         return Response({"data": {"inbox_lead": serializer.data, "inbox_token": firm.inbox_token}, "response": clio_response})
